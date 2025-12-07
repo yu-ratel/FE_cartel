9207a19eccfff72a93fc038442570956094cc7ed
yuyoungmin <dbdudals100@gmail.com>
2025-12-07 20:26:12 +0900

    feat: grade 관련 calculateUtils 구현

--- src/utils/calculate.ts
+++ src/utils/calculate.ts
@@ -1,4 +1,4 @@
-import type { CurrencyType } from '@/lib/types';
+import type { CurrencyType, GradePointListItem } from '@/lib/types';
 
 export const convertCurrencyPrice = (
   price: number,
@@ -10,4 +10,33 @@ export const convertCurrencyPrice = (
   }
 
   return Math.round(price * exchangeRate[currency]);
+};
+
+export const calculateNextGrade = (
+  point: number,
+  gradePointList: GradePointListItem[]
+): GradePointListItem | undefined => {
+  const nextGrade = gradePointList.find(item => item.minPoint > point);
+
+  return nextGrade;
+};
+
+export const calculateGradeProgress = (point: number, gradePointList: GradePointListItem[]): number => {
+  const nextGrade = calculateNextGrade(point, gradePointList);
+
+  if (!nextGrade) {
+    return 1;
+  }
+
+  return Math.round((point / nextGrade.minPoint) * 100) / 100;
+};
+
+export const pointsToNextGrade = (point: number, gradePointList: GradePointListItem[]): number => {
+  const nextGrade = calculateNextGrade(point, gradePointList);
+
+  if (!nextGrade) {
+    return 0;
+  }
+
+  return nextGrade.minPoint - point;
 };
