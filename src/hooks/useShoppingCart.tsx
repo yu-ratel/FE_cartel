644809dbce0b54823c4a03fc06da22a7bbcff2b4
yuyoungmin <dbdudals100@gmail.com>
2025-12-14 17:35:24 +0900

    feat: 상품상세 장바구니 버튼 관리 기능 구현 4. 상품의 상황에 따라 장바구니 버튼에 아래 내용을 출력해주세요 [4.1, 4.2, 4.3] 6. 장바구니에 같은 상품이 있을 경우 '장바구니에서 제거 버튼 클릭 시' 해당상품을 장바구니에서 삭제해 주세요.

--- src/hooks/useShoppingCart.tsx
+++ src/hooks/useShoppingCart.tsx
@@ -1,5 +1,6 @@
 import { useShoppingCart, useSetShoppingCart } from '@/providers/ShoppingCartProvider';
 import { type Product } from '@/lib/types/products';
+import { getItemQuantity, isMoreThanStock, isLessThanStock } from '@/utils/shoppingCart';
 
 export const useShoppingCartController = () => {
   const { shoppingCart } = useShoppingCart();
@@ -7,6 +8,10 @@ export const useShoppingCartController = () => {
 
   const addItem = (product: Product): void => {
     setShoppingCart(prev => [...prev, product]);
+  };
+
+  const addItems = (products: Product[]): void => {
+    setShoppingCart(prev => [...prev, ...products]);
   };
 
   const removeItem = (productId: number): void => {
@@ -21,19 +26,32 @@ export const useShoppingCartController = () => {
     });
   };
 
-  const readItemCount = (product: Product): number => {
-    return shoppingCart.filter(item => item.id === product.id).length;
+  const removeItems = (productId: number): void => {
+    setShoppingCart(prev => {
+      return prev.filter(item => item.id !== productId);
+    });
+  };
+
+  const readItemQuantity = (product: Product): number => {
+    return getItemQuantity(shoppingCart, product);
   };
 
   const isItemMoreThanStock = (product: Product): boolean => {
-    const productCount = readItemCount(product);
-    return product.stock <= productCount;
+    return isMoreThanStock(shoppingCart, product);
   };
 
   const isItemLessThanStock = (product: Product): boolean => {
-    const productCount = readItemCount(product);
-    return productCount <= 0;
+    return isLessThanStock(shoppingCart, product);
   };
 
-  return { addItem, removeItem, readItemCount, isItemMoreThanStock, isItemLessThanStock };
+  return {
+    shoppingCart,
+    addItem,
+    addItems,
+    removeItem,
+    removeItems,
+    readItemQuantity,
+    isItemMoreThanStock,
+    isItemLessThanStock,
+  };
 };
--- src/pages/ProductDetailPage/components/ProductInfoSection.tsx
+++ src/pages/ProductDetailPage/components/ProductInfoSection.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Button, Counter, RatingGroup, Spacing, Text } from '@/ui-lib';
 import { Box, Divider, Flex, Stack, styled } from 'styled-system/jsx';
 import { useShoppingCartController } from '@/hooks/useShoppingCart';
@@ -7,16 +8,58 @@ import { useExchangeRate } from '@/lib/api/common';
 import Tag, { type TagType } from '@/ui-lib/components/tag';
 import { convertCurrencyPrice } from '@/utils/calculate';
 import { formatCurrencyPrice } from '@/utils/format';
+import { getItemQuantity, isMoreThanStock, isLessThanStock } from '@/utils/shoppingCart';
+
+// 아쉬운점
+// 현재는 배열로해서 item, items 로 관리하고 있지만 객체로 관리하는게 좋을까?
+// 객체로 관리하면 상품의 수량을 관리할 수 있고, 배열로 관리하면 상품의 수량을 관리할 수 없음
+// shoppingCart와 임시 장바구니가 필요한 로직을 순수함수로 빼서 유틸로 만들어서 사용하는게 맞는걸까
 
 type ProductInfoSectionProps = {
   product: Product;
 };
 
 function ProductInfoSection({ product }: ProductInfoSectionProps) {
-  const { addItem, removeItem, readItemCount, isItemMoreThanStock, isItemLessThanStock } = useShoppingCartController();
+  const { shoppingCart, addItems, removeItems } = useShoppingCartController();
   const { id, name, category, rating, price, stock } = product;
   const { currency } = useCurrencyContext();
   const exchangeRate = useExchangeRate();
+  const hasProductInShoppingCart = shoppingCart.some(item => item.id === id);
+  const [tmpShoppingCart, setTmpShoppingCart] = useState<Product[]>([]);
+
+  const handleCounterDisabled = (callback: () => boolean): boolean => {
+    if (hasProductInShoppingCart) {
+      return true;
+    }
+
+    return callback();
+  };
+
+  const shoppingCartAction = {
+    tmpAdd: () => setTmpShoppingCart(prev => [...prev, product]),
+    tmpRemove: () => setTmpShoppingCart(prev => prev.slice(0, -1)),
+    add: () => addItems(tmpShoppingCart),
+    remove: () => {
+      removeItems(id);
+      setTmpShoppingCart([]);
+    },
+  };
+
+  const ShoppingCartButton = () => {
+    if (hasProductInShoppingCart) {
+      return (
+        <Button fullWidth color="primary" size="lg" onClick={() => shoppingCartAction.remove()}>
+          장바구니에서 제거
+        </Button>
+      );
+    }
+
+    return (
+      <Button fullWidth color="primary" size="lg" onClick={() => shoppingCartAction.add()}>
+        장바구니 담기
+      </Button>
+    );
+  };
 
   return (
     <styled.section css={{ bg: 'background.01_white', p: 5 }}>
@@ -45,18 +88,22 @@ function ProductInfoSection({ product }: ProductInfoSectionProps) {
           </Text>
         </Flex>
         <Counter.Root>
-          <Counter.Minus onClick={() => removeItem(id)} disabled={isItemLessThanStock(product)} />
-          <Counter.Display value={readItemCount(product)} />
-          <Counter.Plus onClick={() => addItem(product)} disabled={isItemMoreThanStock(product)} />
+          <Counter.Minus
+            onClick={() => shoppingCartAction.tmpRemove()}
+            disabled={handleCounterDisabled(() => isLessThanStock(tmpShoppingCart, product))}
+          />
+          <Counter.Display value={getItemQuantity(tmpShoppingCart, product)} />
+          <Counter.Plus
+            onClick={() => shoppingCartAction.tmpAdd()}
+            disabled={handleCounterDisabled(() => isMoreThanStock(tmpShoppingCart, product))}
+          />
         </Counter.Root>
       </Flex>
 
       <Spacing size={5} />
 
       {/* 장바구니 버튼 */}
-      <Button fullWidth color="primary" size="lg">
-        장바구니
-      </Button>
+      <ShoppingCartButton />
     </styled.section>
   );
 }
